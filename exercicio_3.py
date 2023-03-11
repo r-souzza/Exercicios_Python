13c9fa68199f8f362cfb52b8d47181edc1f4b2ad
Roger Souza <rsouzza.2016@gmail.com>
2023-03-11 17:51:04 -0300

    Exercicio 3 da lista de exercicios

---
+++ exercicio_3.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+num1 = input('Digite um valor: ')
+num1 = int(num1)
+num2 = int(input('Digite outro valor: '))
+total = num1 + num2
+print(f'A soma entre {num1} e {num2} é igual a {total}')
+
