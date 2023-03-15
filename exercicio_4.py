b1980236bf814314351b1c1fa3280bac004c3afc
Roger Souza <rsouzza.2016@gmail.com>
2023-03-14 23:03:08 -0300

    Analisando uma variável

---
+++ exercicio_4.py
@@ -0,0 +1,15 @@
+"""
+Faça um programa que leia algo pelo teclado e mostre na tela
+o seu tipo primitivo e todas as informações possíveis sobre ele.
+"""
+
+dados = input('Digite algo: ')
+
+print('O tipo primitivo desse valor é', type(dados))
+print('Só tem espaços?', dados.isspace())
+print('É um número?', dados.isnumeric())
+print('É alfabético?', dados.isalpha())
+print('É alfanumérico?', dados.isalnum())
+print('Está em maiúsculas?', dados.isupper())
+print('Está em minúsculas?', dados.islower())
+print('Está capitalizada?', dados.istitle()) # CAPITALIZADA (istitle) - Primeira letra maiúscula
